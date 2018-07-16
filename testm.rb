a587d3bfaa34a3f76b32022f8af39228aabcdbec
kamus benayad <kamel@benayad.ovh>
2018-07-17 00:12:31 +0200

    jet_sol_monkey

---
+++ testm.rb
@@ -0,0 +1,33 @@
+exiger  " pry "
+
+ Utilisateur de la classe
+
+  def  set_name_to ( some_string )
+     @name  = fin de chaîne
+
+
+  def  get_name
+    return  @name
+  fin
+
+  def  saluer
+    met  « Salut! Mon nom est # { @name } ! »
+   fin
+
+fin
+
+liaison .pry
+#liaison pry
+met  " fin de fichier "
+
+#Utilisateur.new
+alice =  Utilisateur .new
+alice.set_name_to ( " alice " )
+ met alice.get_name
+
+madhatter =  Utilisateur . nouveau
+madhatter.set_name_to ( " Mad Hatter " )
+ met madhatter.get_name
+
+alice.greet
+madhatter.greet
